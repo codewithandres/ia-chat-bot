00daf998b15be8118e1ce468c1c7b3d0d87f8486
Dize dev <axiozweb2022@gmail.com>
2025-01-01 16:08:33 -0500

    se cambio por una arrow function

--- src/utils/emjojinPiker.ts
+++ src/utils/emjojinPiker.ts
@@ -2,19 +2,17 @@ import type { EmojinPikerOptionType, EmojinMArtType } from '../interfaces';
 
 export const EmojinPikerOption = (
     messageInput: HTMLTextAreaElement
-): EmojinPikerOptionType => {
-    return {
-        theme: 'light',
-        skinTonePosition: 'none',
-        previewPosition: 'none',
-        onEmojiSelect: (emoji: EmojinMArtType) => {
-            messageInput.value += emoji.native;
-            messageInput.focus();
-        },
-        onClickOutside: ({ target }: MouseEvent) => {
-            (target as HTMLElement).closest('#emoji-mart')?.id === 'emoji-mart'
-                ? document.body.classList.toggle('show-emoji-picker')
-                : document.body.classList.remove('show-emoji-picker');
-        },
-    };
-};
+): EmojinPikerOptionType => ({
+    theme: 'light',
+    skinTonePosition: 'none',
+    previewPosition: 'none',
+    onEmojiSelect: (emoji: EmojinMArtType) => {
+        messageInput.value += emoji.native;
+        messageInput.focus();
+    },
+    onClickOutside: ({ target }: MouseEvent) => {
+        (target as HTMLElement).closest('#emoji-mart')?.id === 'emoji-mart'
+            ? document.body.classList.toggle('show-emoji-picker')
+            : document.body.classList.remove('show-emoji-picker');
+    },
+});
