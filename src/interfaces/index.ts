7c67f512dcfe62f98b6b101b61e81ee5ffe9735d
Dize dev <axiozweb2022@gmail.com>
2025-01-01 15:19:50 -0500

    se creo la interfas para EmojinPikerOptionType

--- src/interfaces/index.ts
+++ src/interfaces/index.ts
@@ -6,3 +6,11 @@ export interface UserData {
         mime_type: string;
     };
 }
+export interface EmojinPikerOptionType {
+    theme: string;
+    skinTonePosition: string;
+    previewPosition: string;
+    onEmojiSelect: (emoji: EmojinMArtType) => void;
+    onClickOutside: ({ target }: MouseEvent) => void;
+}
+export type EmojinMArtType = EmojiMart.Emoji;
