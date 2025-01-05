8b3a9a07e11ed55e737ce81c816d5ec4e22ed8d1
Dize dev <axiozweb2022@gmail.com>
2025-01-05 00:27:29 -0500

    se corrio un bug

--- src/components/messageText.ts
+++ src/components/messageText.ts
@@ -2,7 +2,7 @@ import type { UserData } from '../interfaces';
 
 export const MessageContent = ({ file }: UserData): string => {
     return `
-    <div class="message-text">  </div>
+    <div class="message-text" style="white-space: pre-wrap;"></div>
     ${
         file.data &&
         `<img src="data:${file.mime_type};base64,${file.data}" class="attachment" />`
