78c46e19432e29361366c25a13525999d9941277
Dize dev <axiozweb2022@gmail.com>
2025-01-05 00:27:57 -0500

    se valido que exista el elemento html

--- src/screens/renderBotResponse.ts
+++ src/screens/renderBotResponse.ts
@@ -1,25 +1,22 @@
 import { generateBotResponse } from '../services/getResponseBot';
 import { userData } from '../utils/userData';
 
-export const renderBotResponse = async (
-    element: HTMLElement
-): Promise<void> => {
-    const messageElement =
-        element.querySelector<HTMLDivElement>('.message-text');
+export const renderBotResponse = async (element: HTMLElement): Promise<void> => {
+    const messageElement = element.querySelector<HTMLDivElement>('.message-text');
+
+    if (!messageElement) return;
+
     try {
         const botResponse = await generateBotResponse();
 
-        messageElement!.textContent = botResponse;
+        messageElement.textContent = botResponse;
     } catch (error: any) {
-        messageElement!.textContent = ` Sorry, I encountered an error. Please try again ${error.message};`;
-        messageElement!.style.color = '#ff0000';
+        messageElement.style.color = '#ff0000';
     } finally {
         userData.file = { data: '', mime_type: '' };
         element.classList.remove('thinking');
 
-        const chatBody = document.querySelector(
-            ' .chat-body '
-        ) as HTMLDivElement;
+        const chatBody = document.querySelector(' .chat-body ') as HTMLDivElement;
 
         chatBody.scrollTo({ top: chatBody.scrollHeight, behavior: 'smooth' });
     }
